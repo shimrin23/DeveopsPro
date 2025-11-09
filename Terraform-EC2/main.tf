c494b0080f126ccb2cbf28aa19675ff4b8917672
shimrin <139621909+shimrin23@users.noreply.github.com>
2025-11-10 01:59:27 +0530

    new

--- Terraform-EC2/main.tf
+++ Terraform-EC2/main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 resource "aws_instance" "my_ec2" {
   ami           = "ami-0dee22c13ea7a9a67"  
-  instance_type = "t2.micro"
+  instance_type = "t3.micro"
   key_name      = "my-key"   
 
   tags = {
