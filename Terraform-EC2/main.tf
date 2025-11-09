ccce62e6b41a0cbecca50499901d419e17b77e38
shimrin <139621909+shimrin23@users.noreply.github.com>
2025-11-10 01:52:33 +0530

    add key pair

--- Terraform-EC2/main.tf
+++ Terraform-EC2/main.tf
@@ -5,7 +5,7 @@ provider "aws" {
 resource "aws_instance" "my_ec2" {
   ami           = "ami-0dee22c13ea7a9a67"  
   instance_type = "t2.micro"
-  key_name      = "id_rsa"   
+  key_name      = "my-key"   
 
   tags = {
     Name = "MyEC2Instance"
