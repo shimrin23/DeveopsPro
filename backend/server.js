6554002a813ecb4317373dc1387e0d28b5ff4641
shimrin <139621909+shimrin23@users.noreply.github.com>
2026-02-12 04:01:26 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,25 @@
 const express = require("express");
-require("dotenv").config(); 
+require("dotenv").config();
 require("./db/conn");
 const userRouter = require("./routes/userRoutes");
 const stylistRouter = require("./routes/stylistRoutes");
 const appointRouter = require("./routes/appointRoutes");
-const corsMiddleware = require("./middleware/cors");
+const cors = require('cors');
 const path = require("path");
 
 const app = express();
 const port = process.env.PORT || 5001;
 
-app.use(corsMiddleware);
+// Allow requests from your frontend IP
+app.use(cors({
+  origin: 'http://54.227.156.82', // No trailing slash!
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true
+}));
 app.use(express.json());
 
 
-app.use("/api/users", userRouter); 
+app.use("/api/users", userRouter);
 app.use("/api/stylist", stylistRouter);
 app.use("/api/appointment", appointRouter);
 
