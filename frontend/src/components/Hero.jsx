e01bd8c649f35b55e6f63b072ed07b51e04868c1
shimrin <139621909+shimrin23@users.noreply.github.com>
2026-02-15 14:12:11 +0530

    Update Hero.jsx

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -7,7 +7,7 @@ const Hero = () => {
     <section className="hero">
       <div className="hero-content">
         <h1>
-          Elegance Meets <br />
+          Shimrin's <br />
           <span style={{ color: 'var(--accent-color)' }}>Excellence</span>
         </h1>
         <p>
